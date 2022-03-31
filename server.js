1e08ee3bbe51701bd0a4af0c1aae33d6b96210a5
natan-mb <78055688+natan-mb@users.noreply.github.com>
2022-03-30 20:17:14 -0400

    modified the 'covid deaths' endpoint

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
-const { response } = require('express')
 const express = require('express')
 const { fstat } = require('fs')
 const args = require('minimist')(process.argv.slice(0))
 const app = express()
 const fs = require('fs')
 const jsonfile = require('jsonfile')
-let XMLHttpRequest = require('xhr2')
+
 const { JSDOM } = require( "jsdom" );
 const { window } = new JSDOM( "" );
 const $ = require( "jquery" )( window );
-let request = new XMLHttpRequest()
 
 var port = args.port || 3000
 
@@ -17,25 +15,23 @@ const server = app.listen(port, (req, res) => {
     console.log(`App listening on port ${port}`)  
 })
 
-app.get('/covid_deaths', (req, res) => {
-    $.ajax({
-        url: 'https://data.cdc.gov/resource/9bhg-hcku.json',
-        type: 'GET',
+app.get('/covid_deaths/', (req, res) => {
+
+    $.ajax({ // fetches dataset from the CDC website and saves it as a json file
+
+        url: "https://data.cdc.gov/resource/9bhg-hcku.json",
+        type: "GET",
         data: {
-            "$limit": 500,
-            "$app_token": 'LkodRSqKWrJ9i691KYiUPv9oG'
+          "$limit" : 100000,
+          "$$app_token" : "LkodRSqKWrJ9i691KYiUPv9oG"
         }
     }).done(function(data) {
-        alert("retrieved " + data.length + " records from dataset")
-        console.log(data.length)
-    })
-    // jsonfile.readFile(file, function (err, obj) {
-    //     if (err) {
-    //         console.log(err)
-    //     } else {
-    //         console.log(obj)
-    //     }
-    // })
+      console.log("Retrieved " + data.length + " records from the dataset!");
+
+      jsonfile.writeFile('dataset.json', data)
+    });
+
+    res.status(200).send("Successfully retrieved dataset!")
 })
 
 app.use(function(req, res) {
