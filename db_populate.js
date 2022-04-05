f3d7ff51fd0fcf8a092d72af36865131d02dbfe2
natan-mb <78055688+natan-mb@users.noreply.github.com>
2022-04-05 12:35:13 -0400

    added function to update database

--- db_populate.js
+++ db_populate.js
@@ -34,6 +34,7 @@ function covid_deaths_db() {
     );`
     deaths_db.exec(sql)
     console.log('attempting')
+
     $.ajax({ // fetches dataset from the CDC website and saves it as a json file
         url: "https://data.cdc.gov/resource/9bhg-hcku.json",
         type: "GET",
@@ -42,18 +43,20 @@ function covid_deaths_db() {
         "$$app_token" : "LkodRSqKWrJ9i691KYiUPv9oG"
         }
     }).done(function(data) {
-    console.log("Retrieved " + data.length + " records from the dataset!");
-    for(i = 0; i < data.length; i++) {
-        deaths_arr = []
-        this_obj = JSON.stringify(data[i])
-        for(j=0; j < deaths_format.length; j++) {
-            if(data[i].hasOwnProperty(deaths_format[j])) {
-                deaths_arr.push(data[i][deaths_format[j]])
-            } else {
-                deaths_arr.push('')
+        console.log("Retrieved " + data.length + " records from the dataset!");
+
+        // insert dataset into table row by row        
+        for(i = 0; i < data.length; i++) {
+            deaths_arr = []
+            this_obj = JSON.stringify(data[i])
+            for(j=0; j < deaths_format.length; j++) {
+                if(data[i].hasOwnProperty(deaths_format[j])) {
+                    deaths_arr.push(data[i][deaths_format[j]])
+                } else {
+                    deaths_arr.push('')
+                }
             }
-        }
-        deaths_db.prepare(`INSERT INTO covid_deaths (date_as_of,start_date,end_date,group_by,year,month,state,sex,age_group,covid_deaths,total_deaths) VALUES (?,?,?,?,?,?,?,?,?,?,?);`).run(deaths_arr)
+            deaths_db.prepare(`INSERT INTO covid_deaths (date_as_of,start_date,end_date,group_by,year,month,state,sex,age_group,covid_deaths,total_deaths) VALUES (?,?,?,?,?,?,?,?,?,?,?);`).run(deaths_arr)
     }
     console.log('Done')
     }).fail(function(data) {
@@ -61,5 +64,63 @@ function covid_deaths_db() {
     });
 }
 
+// 
+function update_covid_deaths() {
+    
+    let stmt = deaths_db.prepare(`SELECT name FROM sqlite_master where type='table' and name='covid_deaths';`)
+    let row = stmt.get()
+    if(row === undefined) {
+        console.log("failed to update: table 'covid_deaths' does not exist")
+        return
+    }
 
-module.exports = {covid_deaths_db}
+    // get latest end_date from table
+    stmt = deaths_db.prepare(`
+        SELECT MAX(end_date) AS latest
+        FROM covid_deaths;
+    `).get()
+    
+    var latest_date = JSON.parse(stringify(stmt))['latest']
+
+    console.log('fetching rows later than ' + latest_date)
+
+    $.ajax({ // fetches dataset from the CDC website and saves it as a json file
+        url: "https://data.cdc.gov/resource/9bhg-hcku.json?$where=end_date = '" + latest_date + "'",
+        type: "GET",
+        data: {
+        "$limit" : 100000,
+        "$$app_token" : "LkodRSqKWrJ9i691KYiUPv9oG"
+        }
+    }).done(function(data) {
+
+        if (data.length == 0) {
+            console.log('failed to update: no fields later than specified date')
+            return;
+        }
+
+        console.log("Retrieved " + data.length + " records from the dataset!");
+
+        // insert dataset into table row by row        
+        for(i = 0; i < data.length; i++) {
+            deaths_arr = []
+            this_obj = JSON.stringify(data[i])
+            for(j=0; j < deaths_format.length; j++) {
+                if(data[i].hasOwnProperty(deaths_format[j])) {
+                    deaths_arr.push(data[i][deaths_format[j]])
+                } else {
+                    deaths_arr.push('')
+                }
+            }
+            deaths_db.prepare(`INSERT INTO covid_deaths (date_as_of,start_date,end_date,group_by,year,month,state,sex,age_group,covid_deaths,total_deaths) VALUES (?,?,?,?,?,?,?,?,?,?,?);`).run(deaths_arr)
+    }
+    console.log('successfully updated database')
+
+    }).fail(function(data) {
+        console.error('encountered error while updating database')
+        console.error(data)
+    });
+
+    return;
+}
+
+module.exports = {covid_deaths_db, update_covid_deaths}
