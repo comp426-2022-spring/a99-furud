8acd04647fb39e64ae17563f0aaa7f1f8e74fc55
natan-mb <78055688+natan-mb@users.noreply.github.com>
2022-04-18 22:33:00 -0400

    restructured code

--- data.js
+++ data.js
@@ -1,14 +1,16 @@
-const database = require('better-sqlite3')
+const Database = require('better-sqlite3');
+const db = new Database('./database.db');
 
-function getData(dbase, cols=[], paras=[]) {
-    db = new database(dbase + ".db");
+function getData(tbl_name, cols=[], paras=[]) {
+    
     sql = "SELECT ";
+
     if(cols.length == 0) {
         sql = sql + "*";
     } else {
         sql = sql + cols.toString();
     }
-    sql = sql + " FROM " + dbase;
+    sql = sql + " FROM " + tbl_name;
     if(paras.length > 0) {
         sql = sql + " WHERE " + paras.toString();
     }
