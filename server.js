d7027cb3f3ca025e4b069d4cef001a0c39e021e8
natan-mb <78055688+natan-mb@users.noreply.github.com>
2022-04-18 22:32:39 -0400

    endpoints use the new functions

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ const express = require('express')
 const args = require('minimist')(process.argv.slice(0))
 const app = express()
 const fs = require('fs')
-const db_populate = require('./db_populate.js')
 const { JSDOM } = require( "jsdom" );
 const { window } = new JSDOM( "" );
 const $ = require( "jquery" )( window );
 const get_data = require('./data.js')
+
+const db = require('./populate_db.js')
 
 var port = args.port || 3000
 
@@ -22,12 +23,16 @@ app.get('/db_populate.js', (req, res) => {
     res.status(200).send(db_populate.covid_deaths_db())
 })
 
-app.get('/update/', (req, res) => {
-    res.status(200).send(db_populate.update_covid_deaths())
+app.get('/update/:table', (req, res) => {
+    if (req.params.table == 'all') {
+        res.status(200).send(db.update_database())
+    } else {
+        res.status(200).send(db.update_table(req.params.table))    
+    }
 })
 
 app.get('/get_data/', (req, res) => {
-    res.status(200).send(get_data.getData("covid_deaths_over_time"))
+    res.status(200).send(get_data.getData('covid_deaths_over_time', colos=[], paras=''))
     
 })
 
