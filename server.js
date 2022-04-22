590e9a99e54df56f79cd2a64db5ba73d0d3c61b6
natan-mb <78055688+natan-mb@users.noreply.github.com>
2022-04-22 18:59:46 -0400

    '/get_data/' is now a 'POST' endpoint

--- server.js
+++ server.js
@@ -1,44 +1,54 @@
-const express = require('express')
-const args = require('minimist')(process.argv.slice(0))
-const app = express()
-const fs = require('fs')
-const { JSDOM } = require( "jsdom" );
-const { window } = new JSDOM( "" );
-const $ = require( "jquery" )( window );
-const get_data = require('./src/data.js')
-const plotter = require('./plotter.js')
+const express = require("express");
+const args = require("minimist")(process.argv.slice(0));
+const app = express();
+const fs = require("fs");
+const { JSDOM } = require("jsdom");
+const { window } = new JSDOM("");
+const $ = require("jquery")(window);
+const get_data = require("./src/data.js");
 
-app.use(express.static('./frontend'))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-const db = require('./src/populate_db.js')
+app.use(express.static("./frontend"));
 
-var port = args.port || 3000
+const db = require("./src/populate_db.js");
+
+var port = args.port || 3000;
 
 const server = app.listen(port, (req, res) => {
-    console.log(`App listening on port ${port}`)  
-})
+  console.log(`App listening on port ${port}`);
+});
 
-app.get('/covid_deaths/', (req, res) => {
-    res.status(200).send('OK')
-})
+app.get("/covid_deaths/", (req, res) => {
+  res.status(200).send("OK");
+});
 
-app.get('/db_populate.js', (req, res) => {
-    res.status(200).send(db_populate.covid_deaths_db())
-})
+app.get("/db_populate.js", (req, res) => {
+  res.status(200).send(db_populate.covid_deaths_db());
+});
 
-app.get('/update/:table', (req, res) => {
-    if (req.params.table == 'all') {
-        res.status(200).send(db.update_database())
-    } else {
-        res.status(200).send(db.update_table(req.params.table))    
-    }
-})
+app.get("/update/:table", (req, res) => {
+  if (req.params.table == "all") {
+    res.status(200).send(db.update_database());
+  } else {
+    res.status(200).send(db.update_table(req.params.table));
+  }
+});
 
-app.get('/get_data/', (req, res) => {
-    res.status(200).send(get_data.getData('covid_deaths_over_time', cols=['submission_date', 'tot_death'], paras=''))
-    
-})
+app.post("/get_data/", (req, res) => {
+  res
+    .status(200)
+    .send(
+      get_data.getData(
+        req.body.name,
+        (cols = req.body.cols),
+        (paras = req.body.paras),
+        req.body.order
+      )
+    );
+});
 
-app.use(function(req, res) {
-    res.status(404).send('404 Page Not Found')
-})
+app.use(function (req, res) {
+  res.status(404).send("404 Page Not Found");
+});
