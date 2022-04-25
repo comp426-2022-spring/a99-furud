09238c8ec2e87fd6dd8143ab89d53fb7ff223b70
Joey Kalbas <jkalbas@email.unc.edu>
2022-04-24 22:01:11 -0400

    added user_data.db

--- src/middleware/logger.js
+++ src/middleware/logger.js
@@ -1,6 +1,7 @@
 "use strict";
 
-const db = require('../services/logging_db.js');
+const db = require('../services/logging_db.js').db;
+const user_data = require('../services/logging_db.js').user_data;
 
 function logger(req, res, next) {
     let logdata = {
@@ -33,6 +34,50 @@ function logger(req, res, next) {
        `)
 
     stmt.run(logdata)
+    
+    if(req.session.loggedin) {
+        let logdata = {
+            username: req.session.username,
+            remoteaddr: req.ip,
+            remoteuser: req.user,
+            time: Date.now(),
+            method: req.method,
+            url: req.url,
+            protocol: req.protocol,
+            httpversion: req.httpVersion,
+            status: res.statusCode,
+            referer: req.headers['referer'],
+            useragent: req.headers['user-agent']
+        }
+    
+        const st = user_data.prepare(`
+            INSERT INTO user_data(username,
+                remoteaddr,
+                remoteuser,
+                time,
+                method,
+                url,
+                protocol,
+                httpversion,
+                status,
+                referer,
+                useragent)
+            VALUES (
+                @username,
+                @remoteaddr,
+                @remoteuser,
+                @time,
+                @method,
+                @url,
+                @protocol,
+                @httpversion,
+                @status,
+                @referer,
+                @useragent
+        )
+    `)
+    st.run(logdata)
+    }
     next()
 }
 
--- src/services/logging_db.js
+++ src/services/logging_db.js
@@ -2,9 +2,11 @@
 
 const Database = require('better-sqlite3');
 const db = new Database('./data/log/log.db');
+const user_data = new Database('./data/log/user_data.db');
 const stmt = db.prepare(`SELECT name FROM sqlite_master WHERE type='table' and name='accesslog';`);
-
+const st = user_data.prepare(`SELECT name FROM sqlite_master WHERE type='table' and name='user_data';`);
 let row = stmt.get();
+let row2 = st.get();
 
 // Check if table exists
 if (row === undefined) {
@@ -33,4 +35,24 @@ if (row === undefined) {
     console.log('logging database exists.')
 }
 
-module.exports = db
+if (row2 == undefined){
+    const add_table = `CREATE TABLE user_data (
+        actionid INTEGER PRIMARY KEY AUTOINCREMENT, 
+        username VARCHAR NOT NULL,
+        remoteaddr TEXT,
+        remoteuser TEXT,
+        time INTEGER,
+        method TEXT,
+        url TEXT,
+        protocol TEXT,
+        httpversion TEXT,
+        status INTEGER,
+        referer TEXT,
+        useragent TEXT
+    );`
+    user_data.exec(add_table);
+} else {
+    console.log('User data database exists.')
+}
+
+module.exports = {db, user_data}
